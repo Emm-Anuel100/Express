a6e7d2b8c7e9e2aeefc5c9df377c51d72d0babd6
Chinweokwu Emmanuel <125787874+Emm-Anuel100@users.noreply.github.com>
2023-06-04 22:55:17 +0100

    Update er.js

--- er.js
+++ er.js
@@ -1,22 +1,2 @@
 
-// for (let i = 0; i< 5; ++i) {
-//  console.log(i);
-//    }
-
-
-let i = 0;
-  while (i < 10) {
-   console.log("hello bro", ++i);
-   
-  }
-
-
-
-  for (let  i= 0;  i< 20; ++i) { // this will output odd numbers
-   if (i%2 !== 0) {
-      
-      console.log(i);
-   }
-   
-  }
 
